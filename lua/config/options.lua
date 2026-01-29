6a97aa62313d675eb93be75a989b96c226fe8b48
Shalom <shalom2552@gmail.com>
2026-01-30 01:48:42 +0200

    removed inline diagnosis and fixed 'vim undefined global'

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,30 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Tells Lua LSP that 'vim' is a valid global
+if vim.lsp.config then
+  vim.lsp.config("lua_ls", {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+      },
+    },
+  })
+end
+
+-- Automatically opens error details in a popup when the cursor stops moving
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, {
+      focus = false,
+      scope = "cursor",
+      border = "rounded",
+    })
+  end,
+})
+
+-- Hides the inline error messages at the end of lines
+vim.diagnostic.config({ virtual_text = false })
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false, -- This disables the inline text
+      },
+    },
+  },
+}
