927834e4fcea52564b732990a3d07e3cef33174e
Shalom <shalom2552@gmail.com>
2026-01-30 15:47:29 +0200

    fixed extra project in main menu (util.project)

---
+++ lua/plugins/snack.lua
@@ -0,0 +1,12 @@
+-- lua/plugins/snacks.nvim
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      hidden = true, -- Show hidden files in explorer
+    },
+    picker = {
+      hidden = true, -- Show hidden files in picker
+    },
+  },
+}
