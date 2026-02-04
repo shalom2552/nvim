ef61c403870af8b13b80298013c821c8288ceb9a
Shalom <shalom2552@gmail.com>
2026-02-04 20:26:56 +0200

    conf: clangd fix for g++

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,12 +4,21 @@ return {
     opts = {
       diagnostics = {
         virtual_text = false, -- Keep inline errors OFF (as you requested)
+
       },
       servers = {
+        clangd = {
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--query-driver=/usr/bin/g++,/usr/bin/gcc,**", -- Whitelist g++
+          },
+        },
         lua_ls = {
           settings = {
             Lua = {
-              diagnostics = { globals = { "vim" } }, -- Moved from options.lua
+              diagnostics = { globals = { "vim" } }, -- Make "vim" known by LSP
             },
           },
         },
