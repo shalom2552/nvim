33f4a929ac9212738509b25a55bed13585bc2c33
Shalom <shalom2552@gmail.com>
2026-02-04 19:44:47 +0200

    conf: set quiet mode as global

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,31 +1,26 @@
 return {
   "saghen/blink.cmp",
-  -- 1. Blink checks this variable before showing suggestions
   opts = function(_, opts)
     opts.enabled = function()
-      return vim.b.completion ~= false
+      return vim.g.completion ~= false -- Check global variable
     end
     return opts
   end,
 
   keys = {
     {
-      "<leader>uq", -- 'q' for Quiet Mode
+      "<leader>uq",
       function()
-        -- FIXED LOGIC:
-        -- If currently OFF (false), turn ON.
-        -- If currently ON (true) or Default (nil), turn OFF.
-        if vim.b.completion == false then
-          vim.b.completion = true
+        -- Toggle global variable
+        if vim.g.completion == false then
+          vim.g.completion = true
         else
-          vim.b.completion = false
+          vim.g.completion = false
         end
 
-        -- Sync the popup variable
-        vim.g.show_popup = vim.b.completion
+        vim.g.show_popup = vim.g.completion
 
-        -- Toggle Diagnostics based on the new state
-        if vim.b.completion then
+        if vim.g.completion then
           vim.diagnostic.enable()
           print("Quiet Mode: OFF (Helpers ON)")
         else
@@ -33,7 +28,7 @@ return {
           print("Quiet Mode: ON (Helpers OFF)")
         end
       end,
-      desc = "Toggle Quiet Mode (All Helpers)",
+      desc = "Toggle Quiet Mode (Global)",
     },
   },
 }
