7a783f001ade1e6a527bbe6cef2c78fe051c6fd3
Shalom <shalom2552@gmail.com>
2026-02-05 08:56:29 +0200

    config: added to helpers the neo-tree error symbols and change helpera name to IDE-feature

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
   "saghen/blink.cmp",
   opts = function(_, opts)
     opts.enabled = function()
-      return vim.g.completion ~= false -- Check global variable
+      return vim.g.completion ~= false
     end
     return opts
   end,
@@ -11,24 +11,42 @@ return {
     {
       "<leader>uq",
       function()
-        -- Toggle global variable
+        -- 1. Toggle the Global Variable
+        -- logic: if currently false (OFF), turn true (ON). Otherwise (nil or true), turn false.
         if vim.g.completion == false then
           vim.g.completion = true
         else
           vim.g.completion = false
         end
 
+        -- 2. Sync the popup flag
         vim.g.show_popup = vim.g.completion
 
+        -- 3. Apply Changes
         if vim.g.completion then
+          -- TURNING ON
           vim.diagnostic.enable()
-          print("Quiet Mode: OFF (Helpers ON)")
+          print("IDE Features: ON")
         else
+          -- TURNING OFF
           vim.diagnostic.enable(false)
-          print("Quiet Mode: ON (Helpers OFF)")
+          
+          -- This forces Neo-tree to remove the red circles immediately
+          local namespaces = vim.diagnostic.get_namespaces()
+          for ns_id, _ in pairs(namespaces) do
+            vim.diagnostic.reset(ns_id)
+          end
+          
+          print("IDE Features: OFF")
+        end
+
+        -- 4. Refresh Neo-tree to reflect the cleared errors
+        if package.loaded["neo-tree"] then
+           vim.cmd("Neotree refresh")
         end
       end,
-      desc = "Toggle Quiet Mode (Global)",
+      desc = "Toggle IDE Features",
     },
   },
 }
+
