567f943ddb853e46ef2c5b9b71f979e557cfcf1d
Shalom <shalom2552@gmail.com>
2026-04-13 00:31:01 +0300

    conf(theme): set comments and line number color brighter

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,3 +1,4 @@
+-- lua/plugins/tokyonight.lua
 -- Tokyonight theme configuration
 return {
   "folke/tokyonight.nvim",
@@ -5,6 +6,15 @@ return {
     on_highlights = function(hl, c)
       -- override the split separator to be more visible
       hl.WinSeparator = { fg = c.blue }
+
+      -- Brighter line number colors
+      hl.LineNrAbove = { fg = "#65759a", bold = true }
+      hl.LineNrBelow = { fg = "#65759a", bold = true }
+      hl.CursorLineNr = { fg = "#ffc777", bold = true }
+
+      -- Brighter comments color
+      hl.Comment = { fg = "#8a95b0", italic = true }
+
     end,
   },
 }
