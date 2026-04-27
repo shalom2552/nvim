3045d7d1da78a399a941199dbf2c6cc403ed8d39
Shalom <shalom2552@gmail.com>
2026-04-27 23:25:08 +0300

    chore(notes): remove scratch note plugin, now built-in to lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,36 +27,3 @@ vim.keymap.set("n", "<leader>N", function()
   end
 end, { desc = "Notification History" })
 
--- Floating window for scratch note
-vim.keymap.set("n", "<leader>n", function()
-  local path = vim.fn.expand("~/.local/share/nvim/scratch.txt")
-  local buf = vim.fn.bufnr(path)
-  if buf == -1 then
-    buf = vim.api.nvim_create_buf(false, false)
-    vim.api.nvim_buf_set_name(buf, path)
-  end
-  -- hide from bufferline tabs
-  vim.api.nvim_set_option_value("buflisted", false, { buf = buf })
-
-  local win = vim.api.nvim_open_win(buf, true, {
-    relative = "editor",
-    width = math.floor(vim.o.columns * 0.6),
-    height = math.floor(vim.o.lines * 0.6),
-    col = math.floor(vim.o.columns * 0.2),
-    row = math.floor(vim.o.lines * 0.2),
-    border = "rounded",
-    title = " scratch ",
-    title_pos = "center",
-  })
-  vim.cmd("edit " .. path)
-  vim.api.nvim_set_option_value("buflisted", false, { buf = buf })
-
-  local function close()
-    vim.cmd("silent! write")
-    vim.api.nvim_win_close(win, true)
-  end
-
-  vim.keymap.set("n", "q", close, { buffer = buf })
-  vim.keymap.set("n", "<Esc>", close, { buffer = buf })
-end, { desc = "Scratch note (float)" })
-
