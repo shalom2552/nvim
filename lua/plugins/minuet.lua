5b0cdd8ecc9c6373b610c034566ea52c29bdfaf1
shalom2552 <shalom2552@gmail.com>
2026-06-20 21:11:35 +0300

    config(minuet): reduce max token and debaunce time

--- lua/plugins/minuet.lua
+++ lua/plugins/minuet.lua
@@ -8,8 +8,8 @@ return {
         provider = "openai_fim_compatible",
         n_completions = 1,
         context_window = 2048,
-        throttle = 500,
-        debounce = 300,
+        throttle = 50,
+        debounce = 30,
         request_timeout = 15,
         provider_options = {
             openai_fim_compatible = {
@@ -17,11 +17,19 @@ return {
                 name = "Ollama",
                 end_point = "http://localhost:11434/v1/completions",
                 model = os.getenv("OLLAMA_MODEL") or "qwen2.5-coder:1.5b",
-                stream = false,
+                stream = true,
                 optional = {
-                    max_tokens = 256,
+                    max_tokens = 64,
                     top_p = 0.9,
                     temperature = 0.1,
+                    stop = {
+                        "<|endoftext|>",
+                        "<|fim_prefix|>",
+                        "<|fim_middle|>",
+                        "<|fim_suffix|>",
+                        "<|fim_pad|>",
+                        "\n\n", -- Stops generation at double blank lines
+                    },
                 },
             },
         },
