569db48e91ecc9928ee3d7134fd4c70f5de95fde
shalom2552 <shalom2552@gmail.com>
2026-09-17 02:31:58 +0300

    feat(multi-cursor): add plugin

---
+++ lua/plugins/visual-multi.lua
@@ -0,0 +1,9 @@
+return {
+  "mg979/vim-visual-multi",
+  -- Crucial: do not lazy-load this plugin.
+  lazy = false,
+  init = function()
+    -- vim.g.VM_default_mappings = 0 -- Uncomment if you want to turn off default maps
+  end,
+}
+
