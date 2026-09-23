cfecdc40fca6fecd19835a466478eca485742641
shalom2552 <shalom2552@gmail.com>
2026-09-23 22:01:28 +0300

    fix(tmux): use vim navigator inside terminal

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,11 +1,14 @@
 -- Seamless Ctrl+h/j/k/l navigation between Neovim splits and tmux panes.
 return {
-  "christoomey/vim-tmux-navigator",
-  keys = {
-    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>",  desc = "Navigate left" },
-    { "<C-j>", "<cmd>TmuxNavigateDown<cr>",  desc = "Navigate down" },
-    { "<C-k>", "<cmd>TmuxNavigateUp<cr>",    desc = "Navigate up" },
-    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right" },
-  },
-  lazy = false,  -- Ensures it loads immediately
+    init = function()
+        vim.g.tmux_navigator_no_mappings = 1
+    end,
+    "christoomey/vim-tmux-navigator",
+    keys = {
+        { "<C-h>", "<cmd>TmuxNavigateLeft<cr>",  mode = { "n", "t" }, desc = "Navigate left" },
+        { "<C-j>", "<cmd>TmuxNavigateDown<cr>",  mode = { "n", "t" }, desc = "Navigate down" },
+        { "<C-k>", "<cmd>TmuxNavigateUp<cr>",    mode = { "n", "t" }, desc = "Navigate up" },
+        { "<C-l>", "<cmd>TmuxNavigateRight<cr>", mode = { "n", "t" }, desc = "Navigate right" },
+    },
+    lazy = false,  -- Ensures it loads immediately
 }
