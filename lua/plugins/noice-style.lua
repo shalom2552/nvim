28731b3c852a3901967ec5dbf16f2ce4ba2014e5
shalom2552 <shalom2552@gmail.com>
2026-09-23 22:02:13 +0300

    style(noice): use cmd prompt at center

--- lua/plugins/noice-style.lua
+++ lua/plugins/noice-style.lua
@@ -1,20 +1,21 @@
 -- Configure Noice UI for signatures and hover docs
 return {
-  {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      -- Enable borders for LSP popups
-      opts.presets = vim.tbl_deep_extend("force", opts.presets or {}, {
-        lsp_doc_border = true,
-      })
+    {
+        "folke/noice.nvim",
+        opts = function(_, opts)
+            -- Enable borders for LSP popups
+            opts.presets = vim.tbl_deep_extend("force", opts.presets or {}, {
+                lsp_doc_border = true,
+                command_palette = false,
+            })
 
-      -- Apply rounded style and blue diagnostic highlight
-      opts.views = vim.tbl_deep_extend("force", opts.views or {}, {
-        hover = {
-          border = { style = "rounded" },
-          win_options = { winhighlight = "Normal:Normal,FloatBorder:DiagnosticInfo" },
-        },
-      })
-    end,
-  },
+            -- Apply rounded style and blue diagnostic highlight
+            opts.views = vim.tbl_deep_extend("force", opts.views or {}, {
+                hover = {
+                    border = { style = "rounded" },
+                    win_options = { winhighlight = "Normal:Normal,FloatBorder:DiagnosticInfo" },
+                },
+            })
+        end,
+    },
 }
