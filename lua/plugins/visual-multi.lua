873860cd4fad28f22dd4ebd74abc5db8b5494a9b
shalom2552 <shalom2552@gmail.com>
2026-09-23 10:16:26 +0300

    fix(multi-cursor): clear snack keys on exit

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -4,6 +4,22 @@ return {
   lazy = false,
   init = function()
     -- vim.g.VM_default_mappings = 0 -- Uncomment if you want to turn off default maps
+
+
+    -- VM drops blink's <CR>/<Up>/<Down>/<C-b>/<C-f> on exit and blink v1 won't reapply them.
+    -- Clearing the rest makes blink reapply all on next InsertEnter.
+    -- TODO: remove once blink.cmp is v2+ (fixed upstream in saghen/blink.cmp#2266).
+    -- Check version: :Lazy (blink.cmp line) or `git -C ~/.local/share/nvim/lazy/blink.cmp describe --tags`
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "visual_multi_exit",
+      callback = function()
+        for _, m in ipairs(vim.api.nvim_buf_get_keymap(0, "i")) do
+          if m.desc and m.desc:find("^blink%.cmp") then vim.keymap.del("i", m.lhs, { buffer = 0 }) end
+        end
+      end,
+    })
+
+
   end,
 }
 
