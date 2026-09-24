2fcbdec075c13dd9ceee77f46f3392499e2b9a76
shalom2552 <shalom2552@gmail.com>
2026-09-24 16:48:06 +0300

    fix(noice): use darker bg

--- lua/plugins/noice-style.lua
+++ lua/plugins/noice-style.lua
@@ -3,19 +3,29 @@ return {
     {
         "folke/noice.nvim",
         opts = function(_, opts)
-            -- Enable borders for LSP popups
+
+            -- Noice presents: premade on/off features
             opts.presets = vim.tbl_deep_extend("force", opts.presets or {}, {
-                lsp_doc_border = true,
-                command_palette = false,
+                lsp_doc_border  = true,  -- border on LSP hover/signature popups
+                command_palette = false, -- cmd prompt at the middle of screen
             })
 
-            -- Apply rounded style and blue diagnostic highlight
+            -- Noice views: look of each popup type (border, colors, size, position)
             opts.views = vim.tbl_deep_extend("force", opts.views or {}, {
-                hover = {
-                    border = { style = "rounded" },
-                    win_options = { winhighlight = "Normal:Normal,FloatBorder:DiagnosticInfo" },
+                hover = { -- hover: functions signature helps and (K) popups
+                    -- Set transparent background for popup and float border
+                    -- win_options = { winhighlight = "Normal:Normal,FloatBorder:Normal", },
                 },
             })
+
+            -- Noice routes: where each message routes to
+            opts.routes = vim.list_extend(opts.routes or {}, {
+                { -- output shell messages to popup (e.g. :!ls, :!make)
+                    filter = { event = "msg_show", kind = { "shell_out", "shell_err" } },
+                    view = "popup"
+                },
+            })
+
         end,
     },
 }
