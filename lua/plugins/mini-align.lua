5eaf820394638117660a0798b348abfb2f11269b
shalom2552 <shalom2552@gmail.com>
2026-09-24 13:57:16 +0300

    feat(align): add mini-align plugin

---
+++ lua/plugins/mini-align.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "nvim-mini/mini.align",
+        version = "*",
+        opts = {
+            mappings = {
+                start = "ga",
+                start_with_preview = "gA",
+            },
+        },
+    },
+}
