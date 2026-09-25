f145a8703ab92d435565239c28a9849b95f2a2ee
shalom2552 <shalom2552@gmail.com>
2026-09-26 01:01:05 +0300

    fix(tmux): fix tmux navigation on snack explorer

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -1,3 +1,20 @@
+
+-- Explorer floats inside a root split, which breaks vim-tmux-navigator
+-- Navigate from the root split instead: nvim window in that direction if any, else the tmux pane.
+local function explorer_nav(dir, tmux_flag)
+    return function(picker)
+        local target = vim.api.nvim_win_call(picker.layout.root.win, function()
+            local nr = vim.fn.winnr(dir)
+            return nr ~= vim.fn.winnr() and vim.fn.win_getid(nr) or nil
+        end)
+        if target then
+            vim.api.nvim_set_current_win(target)
+        elseif vim.env.TMUX then
+            vim.fn.system({ "tmux", "select-pane", tmux_flag })
+        end
+    end
+end
+
 return {
     "folke/snacks.nvim",
     opts = {
@@ -25,6 +42,7 @@ return {
             -- Show hidden files in picker
             hidden = true,
             -- Centered bordered popup for all pickers using the vscode preset
+            -- (command history, search history, etc.)
             layouts = {
                 vscode = {
                     layout = {
@@ -42,16 +60,34 @@ return {
                     },
                 },
             },
+
             sources = {
                 files = {
                     -- Show hidden files in 'find files'
                     hidden = true,
                 },
+
+                -- override navigations to fix tmux navigations on snack explorer
+                explorer = {
+                    actions = {
+                        nav_left = explorer_nav("h", "-L"),
+                        nav_right = explorer_nav("l", "-R"),
+                    },
+                    win = {
+                        list = {
+                            keys = {
+                                ["<c-h>"] = "nav_left",
+                                ["<c-l>"] = "nav_right",
+                            },
+                        },
+                    },
+                },
             },
+
         },
 
+        -- Show hidden files in explorer
         explorer = {
-            -- Show hidden files in explorer
             hidden = true,
         },
 
